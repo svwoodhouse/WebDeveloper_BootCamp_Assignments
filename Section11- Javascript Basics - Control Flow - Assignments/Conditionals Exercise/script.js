f7fa8d7382ca9113187854f8b020032b142c983e
svwoodhouse <svwoodhouse@gmail.com>
2020-04-08 19:48:07 -0400

    Added Conditional Exercise

---
+++ Section11- Javascript Basics - Control Flow - Assignments/Conditionals Exercise/script.js
@@ -0,0 +1,29 @@
+var age = prompt("What is your age?");
+
+if(age < 0){
+    console.log("Error");
+}
+
+else if(age < 18){
+	console.log("Sorry, you are not old enough to enter the venue");
+}
+
+else if(age < 21) {
+	console.log("You can enter, but cannot drink");
+}
+
+else {
+	console.log("Come on in. You can drink.");
+}
+
+if (age == 21) {
+    console.log("Happy Birthday!!");
+} 
+
+if (age % 2 !== 0) {
+    console.log("Your age is odd!");
+}
+
+if(age % Math.sqrt(age) === 0) {
+    console.log("Your age is a perfect square!");
+}
