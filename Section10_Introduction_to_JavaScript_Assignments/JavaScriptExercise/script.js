de0dae224a851c3ce765fa3dae1040137058e663
svwoodhouse <svwoodhouse@gmail.com>
2020-04-08 15:28:37 -0400

    Finished Javascript Stalker exercise

---
+++ Section10_Introduction_to_JavaScript_Assignments/JavaScriptExercise/script.js
@@ -0,0 +1,8 @@
+var fName = prompt("What is your first name?");
+var lName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+
+alert("Your full name is " + fName + " " + lName + ".");
+alert("You are " + age + " years old");
+console.log("Your full name is " + fName + " " + lName + ".");
+console.log("You are " + age + " years old");
