834b9ebe82d02f3ee7986d35914c57e13f2f8bda
svwoodhouse <svwoodhouse@gmail.com>
2020-04-08 17:53:15 -0400

    Finished Age Calculator program

---
+++ Section10_Introduction_to_JavaScript_Assignments/Age Calculator/script.js
@@ -0,0 +1,4 @@
+var age = prompt("How old are you?");
+
+console.log("You have been alive for " + ((age * 365) + (age / 4)) + " days.");
+alert("You have been alive for " + ((age * 365) + (age / 4)) + " days.");
