a564156e4911da8b64d02bbd57c270fc9f1380f7
svwoodhouse <svwoodhouse@gmail.com>
2020-04-11 21:00:05 -0400

    Finished Todo List project

---
+++ Section13-Javascript Basics - Arrays/Todo List/script.js
@@ -0,0 +1,42 @@
+var todos = ["Buy New Turtle"];
+
+function listTodos(){
+    console.log("**********")
+    todos.forEach(function(todo, i) {
+        console.log(i + " : " + todo);
+    });
+    console.log("**********")
+}
+
+function addTodo(){
+    var newTodo = prompt("Enter new todo");
+    todos.push(newTodo);
+    console.log("Added Todo");
+}
+
+function deleteTodo(){
+    var index = prompt("Enter index of todo to delete");
+    todos.splice(index,1);
+    console.log("Deleted Todo");
+}
+
+window.setTimeout(function() {
+    var input = prompt("What would you like to do?")
+
+    while(input !== "quit"){
+        if(input === "list") {
+            listTodos();
+        }
+        else if(input === "new") {
+            addTodo();
+        }
+        else if(input === "delete") {
+            deleteTodo();
+        }
+        input = prompt("What would you like to do?");
+    }
+
+    console.log("OK, YOU QUIT THE APP");
+},500);
+
+
