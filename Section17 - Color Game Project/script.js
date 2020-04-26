4b58abd46ca5c9841c4e4c1a07ad8a6952d1e490
svwoodhouse <svwoodhouse@gmail.com>
2020-04-26 19:42:24 -0400

    Removed branches

--- Section17 - Color Game Project/script.js
+++ Section17 - Color Game Project/script.js
@@ -8,6 +8,48 @@ var rColorSelection = Math.floor(Math.random() * 255);
 var gColorSelection = Math.floor(Math.random() * 255);
 var bColorSelection = Math.floor(Math.random() * 255);
 
+// changes the text to the random RGB values and displays it in the banner
 rColorSpan.textContent = rColorSelection;
 gColorSpan.textContent = gColorSelection;
 bColorSpan.textContent = bColorSelection;
+
+//get easy and hard button
+var easyButton = document.getElementById("easyButton");
+var hardButton = document.getElementById("hardButton");
+
+//add listeners to easy and hard buttons
+easyButtonFunctionality();
+hardButtonFuntionality();
+
+//Function for the easy button containing the functions/actions for this button
+function easyButtonFunctionality()
+{
+    // Listener for mousehover
+    easyButton.addEventListener("mouseover", function() {
+        this.classList.add("selectedButton");
+    });
+
+    //listener for standard mode
+    easyButton.addEventListener("mouseout", function() {
+        this.classList.remove("selectedButton");
+    });
+
+    //listener for onclick
+    easyButton.addEventListener();
+};
+
+function hardButtonFuntionality(){
+    // Listener for mousehover
+    hardButton.addEventListener("mouseover", function() {
+        this.classList.add("selectedButton");
+    });
+
+    //listener for standard mode
+    hardButton.addEventListener("mouseout", function() {
+        this.classList.remove("selectedButton");
+    });
+
+    //listener for onclick
+    hardButton.addEventListener();
+};
+
